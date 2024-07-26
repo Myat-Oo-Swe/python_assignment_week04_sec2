b95d37aba8178994d549de71505db46dbb538fda
Myat-Oo-Swe <myat00swe505@gmail.com>
2024-07-26 11:28:36 +0700

    Describe the changes made in this file

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ print("Reversed:", so.reverse_string(sample_string))
 print("Capitalized:", so.capitalize_string(sample_string))
 print("Lowercase:", so.lowercase_string(sample_string))
 print("Uppercase:", so.uppercase_string(sample_string))
+
+print("This is an update")
